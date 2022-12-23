dc8dac74cd1c2568b6ed00b9df4a9a8e69192492
agamolix <102031132+agamolix@users.noreply.github.com>
2022-12-23 11:14:50 +0100

    Add files via upload

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atrilles <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: atrilles <atrilles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 19:35:18 by atrilles          #+#    #+#             */
-/*   Updated: 2022/02/03 16:27:03 by atrilles         ###   ########.fr       */
+/*   Updated: 2022/12/08 13:24:32 by atrilles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 int main (void) {
 
 	ClapTrap Diana ("Princess Diana");
+	std::cout << "-------------------------------------------" << std::endl;
 	ClapTrap Charles ("King Charles");
-
-	std::cout << std::endl;
+	std::cout << "-------------------------------------------" << std::endl;
 	Diana.attack("King Charles");
-	std::cout << std::endl;
+	std::cout << "-------------------------------------------" << std::endl;
 	Charles.takeDamage(5);
-	std::cout << std::endl;
+	std::cout << "-------------------------------------------" << std::endl;
 	Charles.beRepaired(3);
-	std::cout << std::endl;
+	std::cout << "-------------------------------------------" << std::endl;
 
 	return 0;
 }
