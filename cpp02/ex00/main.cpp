d7aafcabae80cc49a9ae97011a4c70f83951a7cf
agamolix <102031132+agamolix@users.noreply.github.com>
2022-12-23 11:12:37 +0100

    Add files via upload

--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atrilles <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: atrilles <atrilles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 19:35:18 by atrilles          #+#    #+#             */
-/*   Updated: 2022/02/03 16:27:03 by atrilles         ###   ########.fr       */
+/*   Updated: 2022/12/08 13:03:22 by atrilles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,18 @@
 int main (void) {
 
 	Fixed a;
+	std::cout << "------------------------------------------" << std::endl;
 	Fixed b (a);
+	std::cout << "------------------------------------------" << std::endl;
 	Fixed c;
+	std::cout << "------------------------------------------" << std::endl;
 	c = b;
+	std::cout << "------------------------------------------" << std::endl;
 
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
+	std::cout << "------------------------------------------" << std::endl;
 
 	return 0;
 }
