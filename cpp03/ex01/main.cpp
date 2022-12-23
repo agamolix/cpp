c89f1828c615c50e5b894ca1d2ad85f90fc6f3fa
agamolix <102031132+agamolix@users.noreply.github.com>
2022-12-23 11:15:14 +0100

    Add files via upload

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atrilles <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: atrilles <atrilles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 19:35:18 by atrilles          #+#    #+#             */
-/*   Updated: 2022/02/03 16:27:03 by atrilles         ###   ########.fr       */
+/*   Updated: 2022/12/08 13:27:03 by atrilles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,19 @@
 int main (void) {
 
 	ClapTrap Charles ("King Charles");
+	std::cout << "-------------------------------------------" << std::endl;
 	ScavTrap Harry ("Prince Harry");
-	std::cout << std::endl;
-
+	std::cout << "-------------------------------------------" << std::endl;
 	Charles.attack("Prince Harry");
+	std::cout << "-------------------------------------------" << std::endl;
 	Harry.takeDamage(0);
-	std::cout << std::endl;
-
+	std::cout << "-------------------------------------------" << std::endl;
 	Harry.attack("King Charles");
+	std::cout << "-------------------------------------------" << std::endl;
 	Charles.takeDamage(20);
-	std::cout << std::endl;
-
+	std::cout << "-------------------------------------------" << std::endl;
 	Harry.guardGate();
-	std::cout << std::endl;
+	std::cout << "-------------------------------------------" << std::endl;
 
 	return 0;
 }
