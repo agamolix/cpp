2cdaaab50d1dbf0959d16b8fc28445f18c15376a
agamolix <102031132+agamolix@users.noreply.github.com>
2022-12-23 11:13:44 +0100

    Add files via upload

--- cpp02/ex02/Fixed.h
+++ cpp02/ex02/Fixed.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Fixed.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atrilles <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: atrilles <atrilles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 19:35:18 by atrilles          #+#    #+#             */
-/*   Updated: 2022/02/03 16:27:03 by atrilles         ###   ########.fr       */
+/*   Updated: 2022/12/08 13:04:20 by atrilles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,6 @@ class Fixed {
 
 	private:
 	int _value;
-	float _float;
 	static int const _bits = 8;
 };
 
