c413d0bcbc3f66e8e935cd9772e029091c5b4b77
agamolix <102031132+agamolix@users.noreply.github.com>
2022-12-23 11:08:00 +0100

    Add files via upload

--- cpp01/ex00/Zombie.cpp
+++ cpp01/ex00/Zombie.cpp
@@ -3,33 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   Zombie.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atrilles <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: atrilles <atrilles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 19:35:18 by atrilles          #+#    #+#             */
-/*   Updated: 2022/02/03 16:27:03 by atrilles         ###   ########.fr       */
+/*   Updated: 2022/12/08 11:25:36 by atrilles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.h"
 
 Zombie::Zombie(void){
+	this->_name = "unknown";
 	std::cout << "Zombie construit" << std::endl;
 }
 
 Zombie::~Zombie(void){
-	std::cout << "Zombie " << _name << " détruit" << std::endl;
+	std::cout << "Zombie " << this->_name << " détruit" << std::endl;
 }
 
 std::string Zombie::get_name() {
-	return _name;
+	return this->_name;
 }
 
 void Zombie::set_name(std::string name) {
-	_name = name;
+	this->_name = name;
 }
 
 void Zombie::announce(void){
-	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
 
 
