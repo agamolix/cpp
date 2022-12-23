98e5b6eab8466d7946e653ab73eb6bd85680da11
agamolix <102031132+agamolix@users.noreply.github.com>
2022-12-23 11:15:41 +0100

    Add files via upload

--- cpp03/ex02/main.cpp
+++ cpp03/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atrilles <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: atrilles <atrilles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 19:35:18 by atrilles          #+#    #+#             */
-/*   Updated: 2022/02/03 16:27:03 by atrilles         ###   ########.fr       */
+/*   Updated: 2022/12/08 13:29:44 by atrilles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,19 @@
 int main (void) {
 
 	ClapTrap Charles ("King Charles");
+	std::cout << "-------------------------------------------" << std::endl;
 	ScavTrap Harry ("Prince Harry");
+	std::cout << "-------------------------------------------" << std::endl;
 	FragTrap Meghan ("Actress Meghan");
-
-	std::cout << std::endl;
-
+	std::cout << "-------------------------------------------" << std::endl;
 	Meghan.attack("King Charles");
+	std::cout << "-------------------------------------------" << std::endl;
 	Charles.takeDamage(30);
-	std::cout << std::endl;
-
+	std::cout << "-------------------------------------------" << std::endl;
 	Harry.guardGate();
-	std::cout << std::endl;
-
+	std::cout << "-------------------------------------------" << std::endl;
 	Meghan.highFivesGuys();
-	std::cout << std::endl;
+	std::cout << "-------------------------------------------" << std::endl;
 
 	return 0;
 }
